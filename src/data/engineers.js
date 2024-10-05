cde416386b867951a9ec1d03e0f7056a1600ad05
Mary <njuemarywanjiru@gmail.com>
2024-10-05 10:05:43 +0300

    this commits adds my profile and short description

--- src/data/engineers.js
+++ src/data/engineers.js
@@ -1,11 +1,12 @@
 import sampleHeadshotImg from '../assets/sample-headshot.jpg';
+import marynjue from '../pages/MaryNJue.jsx/assets/mary3.jpeg'
 const engineers = [
     {
-        headshot: sampleHeadshotImg,
-        name: 'florah nduati',
-        excerpt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum rem expedita praesentium odio doloremque soluta, quisquam voluptatem iste voluptas excepturi? Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum rem expedita praesentium odio doloremque soluta, quisquam voluptatem iste voluptas excepturi?",
+        headshot: marynjue,
+        name: 'Mary Njue',
+        excerpt: "A motivated Software Engineer graduate with a passion for web development and problem solving skills. Skilled in HTML, CSS and React. I create efficient, user-freindly solution. Always eager to learn and grow in the tech industry",
         portfolioUrl: "/",
-        githubUrl: "https://github.com/codeme254"
+        githubUrl: "https://github.com/MaryNjue"
     },
     {
         headshot: sampleHeadshotImg,
