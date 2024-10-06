400ffcda3b7b881e07f5ec84adaaa117c6b67c38
git <florah@gmail.com>
2024-10-06 20:55:00 +0300

    resolve conflicting errors

---
+++ src/pages/Florah-nduati/Florah.jsx
@@ -0,0 +1,20 @@
+import Heading from "./components/Header/header";
+import Hero2 from "./components/Hero/hero";
+import About from "./components/About/About";
+import Technologies from "./components/Technologies/technologies";
+import Projects from "./components/Projects/projects";
+import Contact from "./components/Contacts/contacts";
+
+function Florah() {
+  return (
+    <>
+      <Heading />
+      <Hero2 />
+      <About />
+      <Technologies />
+      <Projects />
+      <Contact />
+    </>
+  );
+}
+export default Florah;
