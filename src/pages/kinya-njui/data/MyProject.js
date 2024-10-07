68bbef9d8a5c7a670bacba072d70bcdbccfba057
kinya-njui <kinyanjuicharlesm@gmail.com>
2024-10-07 11:43:13 +0300

    Updates on My projects Description

--- src/pages/kinya-njui/data/MyProject.js
+++ src/pages/kinya-njui/data/MyProject.js
@@ -9,7 +9,7 @@ const projectsData = [
     image: project1,
     title: "MUTC web page in React",
     description:
-      "A task management platform using React  to streamline user workflows efficiently.",
+      "A modern and responsive web page designed for the Murang'a University Tech Club, built using React to highlight the club's mission, events, and leadership while offering an engaging user interface.",
     links: {
       liveUrl: "https://muranga-university-tech-club-landing-page-zy1k-pzbqijohq.vercel.app/",
       githubUrl:
@@ -21,7 +21,7 @@ const projectsData = [
     image: project2,
     title: "Urban Haven landing page",
     description:
-      "A sleek landing page for an apartment service, guiding users through the offerings and benefits.",
+      "A beautifully crafted landing page for an upscale apartment complex, showcasing its features, amenities, and contact information in a visually appealing layout, optimized for seamless navigation..",
     links: {
       liveUrl: "https://urban-haven-apartments-j76og3f3s-charles-mburus-projects.vercel.app/",
       githubUrl:
@@ -34,7 +34,7 @@ const projectsData = [
     image: project3,
     title: " React Rock, Paper, Scissors game",
     description:
-      "A fun and interactive game of Rock, Paper, Scissors built in React with smooth user experience.",
+      "An interactive Rock, Paper, Scissors game built with React, featuring dynamic gameplay, vibrant visuals, and responsive design, delivering an enjoyable and engaging user experience.",
     links: {
       liveUrl: "https://rock-paper-scissors-game-7q4oy8auj-charles-mburus-projects.vercel.app/",
       githubUrl: "https://github.com/kinya-njui/Rock-Paper-Scissors-Game",
@@ -46,7 +46,7 @@ const projectsData = [
     image: project4,
     title: "Image Carousel with Html, Css and Javascript",
     description:
-      "An interactive image carousel built with HTML, CSS, and JavaScript to enhance user experience.",
+      "A sleek image slider built using HTML, CSS, and JavaScript, designed to deliver smooth transitions and intuitive controls, enhancing the visual storytelling of any website.",
     links: {
       liveUrl: "https://kinya-njui.github.io/Corousel-Code-Along/",
       githubUrl:
